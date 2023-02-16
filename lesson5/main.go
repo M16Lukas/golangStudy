0b6129c09953cab9641d1d151e85b7915de626a7
unknown <ipark308@gmail.com>
2023-02-16 22:30:51 +0900

    add: セクション6 演算子

---
+++ lesson5/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* ------------------------------------------------------
+	 * 算術演算子
+	------------------------------------------------------*/
+	// 足し算
+	fmt.Println(1 + 2)         //3
+	fmt.Println("ABC" + "CDE") // ABCCDE
+	// 引き算
+	fmt.Println(5 - 1) //4
+	// 掛け算
+	fmt.Println(5 * 4) //20
+	// 割り算
+	fmt.Println(8 / 4) //2
+	// あまりの計算
+	fmt.Println(9 % 4) //1
+
+	// 算術演算子の代入
+	n := 0
+	n += 4         // n = n + 4
+	fmt.Println(n) // 4
+	n++            // n = n + 1
+	fmt.Println(n) // 5
+	n--            // n = n - 1
+	fmt.Println(n) // 4
+
+	s := "ABC"
+	s += "DEF"
+	fmt.Println(s) // ABCDEF
+
+	/* ------------------------------------------------------
+	 * 比較演算子
+	------------------------------------------------------*/
+	fmt.Println(1 == 1)        // true
+	fmt.Println(1 == 2)        // false
+	fmt.Println(4 <= 8)        // true
+	fmt.Println(1 >= 8)        // false
+	fmt.Println(1 < 8)         // true
+	fmt.Println(1 > 8)         // false
+	fmt.Println(true == false) // false
+	fmt.Println(true != false) // true
+
+	/* ------------------------------------------------------
+	 * 論理演算子
+	------------------------------------------------------*/
+	fmt.Println(true && false == true)  // false
+	fmt.Println(true && true == true)   // true
+	fmt.Println(true || true == false)  // true
+	fmt.Println(false || true == false) // false
+	fmt.Println(!true)                  // false
+	fmt.Println(!false)                 // true
+
+}
